a272c9cf3c38570c4c830c8c0f37a0bd34bfd2b3
Villermen <villermen@gmail.com>
2013-10-11 17:19:23 +0200

    Readded set to for Speed.Amplitude & Direction

--- Speed.cs
+++ Speed.cs
@@ -11,31 +11,50 @@ namespace HatlessEngine
         public float X;
         public float Y;
 
+        private float _Direction;
+
         public Speed(float x, float y)
         {
             X = x;
             Y = y;
+            _Direction = 0;
         }
 
         public float Direction
         {
             get 
             {
-                float result;
                 if (X == 0 && Y == 0)
-                    result = 0;
-                else
-                {
-                    result = (float)(Math.Atan2(X, -Y) * 180 / Math.PI);
-                    if (result < 0)
-                        result = result + 360;
-                }
+                    return _Direction;
+
+                float result = (float)(Math.Atan2(X, -Y) * 180 / Math.PI);
+                if (result < 0)
+                    result = result + 360;
                 return result;
+            }
+            set
+            {
+                while (value >= 360)
+                    value = value - 360;
+                while (value < 0)
+                    value = value + 360;
+
+                _Direction = value;
+
+                float amplitude = Amplitude;
+                X = (float)Math.Cos((value / 180 - 0.5) * Math.PI) * amplitude;
+                Y = (float)Math.Sin((value / 180 - 0.5) * Math.PI) * amplitude;
             }
         }
         public float Amplitude
         {
             get { return (float)Math.Abs(Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2))); }
+            set
+            {
+                float direction = Direction;
+                X = (float)Math.Cos((direction / 180 - 0.5) * Math.PI) * value;
+                Y = (float)Math.Sin((direction / 180 - 0.5) * Math.PI) * value;
+            }
         }
     }
 }
