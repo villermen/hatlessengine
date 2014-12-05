43fc20c9028782405348448baadb15b1b31b0c94
Villermen <villermen@gmail.com>
2014-12-05 17:44:37 +0100

    Added AssemblyInfo

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HatlessEngine")]
+[assembly: AssemblyCompany("Villermen")]
+[assembly: AssemblyProduct("HatlessEngine")]
+[assembly: AssemblyCopyright("Copyright © Villermen")]
+[assembly: AssemblyTrademark("HatlessEngine")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("15bb0b2f-9e7c-4b65-83d4-8de4edae417d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.*")]
